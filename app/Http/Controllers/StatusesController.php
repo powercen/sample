9e31b855838f3acbb56bb4f27bcf246dc2b3d867
Power Cen <3565645@qq.com>
2018-03-23 03:31:51 +0000

    Add user statuses

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Status;
 use Illuminate\Http\Request;
 use Auth;
 
@@ -22,6 +23,14 @@ class StatusesController extends Controller
         Auth::user()->statuses()->create(['content' => $request['content']]);
         return redirect()->back();
 
+    }
+
+    public function destroy(Status $status)
+    {
+        $this->authorize('destroy', $status);
+        $status->delete();
+        session()->flash('success', '删除成功');
+        return redirect()->back();
     }
 
 }
